3bab20f0f1839a14fdb22c94dfee35c7416079e1
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-06-20 15:48:12 +0530

    20/6

---
+++ Bootstrap/BootstrapEx/BootstrapEx/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Grid()
+        {
+            return View();
+        }
+
+        public ActionResult Typography()
+        {
+            return View();
+        }
+        public ActionResult SomeComponents()
+        {
+            return View();
+        }
+
+        public ActionResult FewMoreComponents()
+        {
+            return View();
+        }
+
+
+        public ActionResult Tables()
+        {
+            return View();
+        }
+
+        public ActionResult Images()
+        {
+            return View();
+        }
+
+        public ActionResult Cards()
+        {
+            return View();
+        }
+
+        public ActionResult MarginPadding()
+        {
+            return View();
+        }
+
+        public ActionResult Forms()
+        {
+            return View();
+        }
+
+        public ActionResult Navigation()
+        {
+            return View();
+        }
+
+        public ActionResult Navbar()
+        {
+            return View();
+        }
+
+        public ActionResult Helpers()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bootstrap/BootstrapEx/BootstrapEx/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BootstrapEx.Models
+{
+    public class Product
+    {
+        public Int64 ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string MfgName { get; set; }
+        public double Price { get; set; }
+    }
+}
