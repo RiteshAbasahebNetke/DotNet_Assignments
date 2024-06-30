6f3deba222f9e25d74f01b401035155dc871f5cc
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-06-30 19:48:16 +0530

    30/6

---
+++ OOPS/ObjectEx/ObjectEx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectEx
+{
+    internal class Student
+    {
+        public int StudentID;
+        public string StudentName;
+        public string CourseName;
+        public string MobileNo;
+
+        public void AddStudent(int si,string sn,string cn,string mn)
+        {
+            StudentID = si;
+            StudentName = sn;
+            CourseName = cn;
+            MobileNo = mn;
+        }
+        public void DispStudent()
+        {
+            Console.WriteLine("StudentID:" + StudentID);
+            Console.WriteLine("StudentName:" + StudentName);
+            Console.WriteLine("CourseName:" + CourseName);
+            Console.WriteLine("MobileNo:" + MobileNo);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.AddStudent(1, "Raj", "C#", "9984303232");
+            s.DispStudent();
+
+            Student s1 = new Student();
+            s1.AddStudent(2, "Sham", "VB", "3432034309");
+            s1.DispStudent();
+        }
+    }
+}
---
+++ OOPS/abstractionEx/abstractionEx/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractionEx
+{
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+    class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            double area = 3.14 * radius * radius;
+            return area;
+        }
+    }
+    class Rectangle : Shape
+    {
+        private double length;
+        private double bredth;
+        public Rectangle(double length, double bredth)
+        {
+            this.length = length;
+            this.bredth = bredth;
+        }
+
+        public override double CalculateArea()
+        {
+            double a = this.length * this.bredth;
+            return a;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(2);
+            double area=c.CalculateArea();
+            Console.WriteLine("Area of Cirle is:" + area);
+
+            Rectangle r = new Rectangle(2, 3);
+            double a = r.CalculateArea();
+            Console.WriteLine("Area of rectangle is:" + a);
+        }
+    }
+}
