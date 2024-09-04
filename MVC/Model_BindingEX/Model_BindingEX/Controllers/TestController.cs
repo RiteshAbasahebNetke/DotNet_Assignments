3b609f2362e5b92f3ff51798e2b1586137dffc6d
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-09-04 20:43:47 +0530

    4/9

---
+++ MVC/Model_BindingEX/Model_BindingEX/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Model_BindingEX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Model_BindingEX.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create([Bind(Include ="EmailID")]Customer rec)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Model_BindingEX/Model_BindingEX/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Model_BindingEX.Models
+{
+    public class Customer
+    {
+        public Int64 CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string EmailID { get; set; }
+        public string MobileNo { get; set; }
+        public decimal CreditLimit { get; set; }
+    }
+}
