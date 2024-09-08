3e1db4a770f36ccc3f2c2770da120327ff139b12
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-09-08 18:01:52 +0530

    8/9

---
+++ MVC/Child_Action_Call/Child_Action_Call/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Child_Action_Call.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string SayHello()
+        {
+            return "Say Hello Called..!";
+        }
+        public string SayBye()
+        {
+            return "Say Bye Called..!";
+        }
+        public PartialViewResult GetSample()
+        {
+            return PartialView("_Sample");
+        }
+        public PartialViewResult GetDemo()
+        {
+            return PartialView("_Demo");
+        }
+    }
+}
