41733da1647ab2d907271656cf521a6d1f3422f3
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-09-25 18:06:47 +0530

    25/9

---
+++ MVC/Db_First-EX/Db_First-EX/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Db_First_EX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Db_First_EX.Controllers
+{
+    public class TestController : Controller
+    {
+        AEDEntities ae=new AEDEntities();
+        // GET: Test
+        public ActionResult Index()
+        {
+            //var v = from m in ae.EmpTbls
+            //        where m.EmpId>2
+            //        select m;
+
+            var v = from m in ae.EmpTbls
+                    join m1 in ae.DeptTbls
+                    on m.DeptId equals m1.DeptId
+                    select new EmpVM
+                    {
+                        EmpId = m.EmpId,
+                        EmpName = m.EmpName,
+                        EmailId = m.EmailId,
+                        DeptId = m1.DeptId
+                      //  Salary=m.Salary
+                    };
+
+            return View(v.ToList());
+        }
+    }
+}
---
+++ MVC/Db_First-EX/Db_First-EX/Models/EmpVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Db_First_EX.Models
+{
+    public class EmpVM
+    {
+        public Int64 EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmailId { get; set; }
+        public decimal? Salary { get; set; }
+        public  string DeptName { get; set; }
+        public string AreaName { get; set; }
+        public Int64 DeptId { get; set; }
+    }
+}
