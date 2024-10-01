c1ddae55be3f45f82af6efc3959a96330c760229
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-01 18:47:47 +0530

    1/10

---
+++ Core/PartialViewEx/PartialViewEx/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewEx.Models;
+
+namespace PartialViewEx.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Emp e=new Emp();
+            return View(e);
+        }
+    }
+}
---
+++ Core/PartialViewEx/PartialViewEx/Models/Emp.cs
@@ -0,0 +1,20 @@
+﻿namespace PartialViewEx.Models
+{
+    public class Emp
+    {
+        public Int64 EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+        public string EmailId { get; set; }
+        public decimal Salary { get; set; }
+
+        public Emp()
+        {
+            this.EmpID = 1;
+            this.EmpName = "Sujit";
+            this.DeptName = "Sales";
+            this.EmailId = "sujit@hotmail.com";
+            this.Salary = 45000;
+        }
+    }
+}
