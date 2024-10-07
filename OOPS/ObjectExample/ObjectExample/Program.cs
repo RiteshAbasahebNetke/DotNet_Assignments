5cf05ed714282fb2281b982a8292dd3d29912a0c
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-07 21:02:47 +0530

    7/10

---
+++ OOPS/ObjectExample/ObjectExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectExample
+{
+    public class Course
+    {
+        int CourseID;
+        string CourseName;
+        string CourseDuration;
+        double CourseFees;
+
+        public void AddCourse()
+        {
+            CourseID = 12;
+            CourseName = "C#";
+            CourseDuration = "1 month";
+            CourseFees = 2000;
+        }
+        public void DispCourse()
+        {
+            Console.WriteLine("CourseID is:" + CourseID);
+            Console.WriteLine("CourseName is:" + CourseName);
+            Console.WriteLine("CourseDuration is:" + CourseDuration);
+            Console.WriteLine("CourseFees is:" + CourseFees);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course c = new Course();
+            c.AddCourse();
+            c.DispCourse();
+        }
+    }
+}
