3436b2f140a1d5c3249008d9a57cb27b42ea05c0
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-13 18:41:55 +0530

    13/10

---
+++ OOPS/AbstractAreaExample/AbstractAreaExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAreaExample
+{
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    class Circle : Shape
+    {
+        private int r;
+        public Circle(int r)
+        {
+            this.r = r;
+        }
+        public override double CalculateArea()
+        {
+            double a = 3.14 * r * r;
+            return a;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(5);
+            double a=c.CalculateArea();
+            Console.WriteLine("Area of Circle is:" + a);
+        }
+    }
+}
---
+++ OOPS/AbstractRectangleEX/AbstractRectangleEX/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractRectangleEX
+{
+    abstract class Shape
+    {
+        public abstract double CalulateArea();
+    }
+    class Rectangle : Shape
+    {
+        private int l, b;
+        public Rectangle(int l, int b)
+        {
+            this.l = l;
+            this.b = b;
+        }
+
+        public override double CalulateArea()
+        {
+            double a = l * b;
+            return a;..
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(4, 5);
+            double a=r.CalulateArea();
+            Console.WriteLine("Area of Rectangle is:" + a);
+        }
+    }
+}
