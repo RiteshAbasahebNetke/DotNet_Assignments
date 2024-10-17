a455a0475749559d2d6ad4240fabbe6404f50f54
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-17 18:22:42 +0530

    17/10

---
+++ Simple Programs/AdditionOFNumbersEX/AdditionOFNumbersEX/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionOFNumbersEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int add = 0;
+            for(int i=0;i<5;i++)
+            {
+                Console.Write("Enter Number:");
+                num = Convert.ToInt32(Console.ReadLine());
+                add = num + add;
+            }
+            Console.WriteLine("Addition of number is:"+ add);
+        }
+    }
+}
---
+++ Simple Programs/ReplaceZeroEX/ReplaceZeroEX/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceZeroEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, dig;
+            Console.Write("Enter Number:");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Output is:");
+            do
+            {
+                dig = num % 10;
+                if (dig % 2== 0)
+                    Console.Write("0");
+                else
+                    Console.Write(dig);
+                num = num / 10;
+            }
+            while (num > 0);
+
+        }
+    }
+}
