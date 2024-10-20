a997faadffb029d188d49ace50513633463aa0d1
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-20 18:33:45 +0530

    20/10

---
+++ Practice/MinArrayNo/MinArrayNo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinArrayNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[5];
+            for(int i=0;i<5; i++)
+            {
+                Console.Write("Enter No:");
+                n[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            int min = n[0];
+            for(int i=0;i<5;i++)
+            {
+                if(min>n[i])
+                {
+                    min = n[i];
+                }
+            }
+            Console.WriteLine("Minimum number is:" + min);
+        }
+        
+    }
+}
