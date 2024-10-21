e080556abc43b3ebc6292b428db2a18e7b120d44
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-21 18:55:15 +0530

    21/10

---
+++ Simple Programs/StarpatEX/StarpatEX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarpatEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=1;j<5;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
