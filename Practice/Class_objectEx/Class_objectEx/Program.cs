34d4a8c72bdb2f2139d46f97ddffedbd39400cd0
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-24 16:36:10 +0530

    24/10

---
+++ Practice/Class_objectEx/Class_objectEx/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_objectEx
+{
+    internal class Program
+    {
+        class Student
+        {
+            private int StudentID;
+            private string StudentName;
+            private string ClassName;
+            private int RollNo;
+
+            public void AddStudent(int sid,string sn,string cn,int rn)
+            {
+                this.StudentID = sid;
+                this.StudentName = sn;
+                this.ClassName = cn;
+                this.RollNo = rn;
+            }
+
+            public void DispStudent()
+            {
+                Console.WriteLine("StuentID:" + this.StudentID);
+                Console.WriteLine("Student Name:" + this.StudentName);
+                Console.WriteLine("Class Name:" + this.ClassName);
+                Console.WriteLine("Roll No:" + this.RollNo);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student s=new Student();
+            s.AddStudent(1, "Satish", "Fifth", 12);
+            s.DispStudent();
+        }
+    }
+}
---
+++ Practice/ProductEXX/ProductEXX/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductEXX
+{
+    internal class Program
+    {
+        class Product
+        {
+            private int ProductID;
+            private string ProductName;
+            private string MfgName;
+            private double Price;
+
+            public void AddProduct(int pid,string pn,string mn,double p)
+            {
+                this.ProductID = pid;
+                this.ProductName = pn;
+                this.MfgName= mn;
+                this.Price = p;
+            }
+
+            public void DispProduct()
+            {
+                Console.WriteLine("ProductID" + this.ProductID);
+                Console.WriteLine("Product Name:" + this.ProductName);
+                Console.WriteLine("Mfg Name:" + this.MfgName);
+                Console.WriteLine("Price::" + this.Price);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Product[] pr = new Product[3];
+            int pid;
+            string pn, mn;
+            double p;
+            
+            for(int i=0;i<3;i++)
+            {
+                Console.Write("Enter ProductID:");
+                pid = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Product Name:");
+                pn = Console.ReadLine();
+
+                Console.Write("Enter MfgName:");
+                mn= Console.ReadLine();
+
+                Console.Write("Enter Price:");
+                p = Convert.ToDouble(Console.ReadLine());
+
+                pr[i] = new Product();
+                pr[i].AddProduct(pid, pn, mn, p);
+                Console.WriteLine();
+
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                pr[i].DispProduct();
+            }
+        }
+    }
+}
