c4e542e0af95d51ba45c398144ed6350dded3159
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-10-28 16:04:47 +0530

    28/10

---
+++ jQuery/28-10-2024/JQueryEx/JQueryEx/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryEx.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Selectors()
+        {
+            return View();
+        }
+        public ActionResult FewMoreSelectors()
+        {
+            return View();
+        }
+        public ActionResult DomTraversal()
+        {
+            return View();
+        }
+        public ActionResult DomTraversal1()
+        {
+            return View();
+        }
+        public ActionResult Events()
+        {
+            return View();
+        }
+    }
+}
