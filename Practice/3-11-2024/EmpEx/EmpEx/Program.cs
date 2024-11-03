4507708b6e2eccd67667806c00f5dce5b4eaccb0
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-11-03 20:33:14 +0530

    3/11

---
+++ Practice/3-11-2024/EmpEx/EmpEx/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpEx
+{
+    class Emp
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public string DeptName { get; set; }
+        public double Salary { get; set; }
+
+        public void AddEmp(int eid,string en,string dn,double s)
+        {
+            this.EmpID = eid;
+            this.EmpName = en;
+            this.DeptName = dn;
+            this.Salary = s;
+        }
+
+        public void DispEmp()
+        {
+            Console.WriteLine("EmpID:"+ this.EmpID);
+            Console.WriteLine("Emp Name:" + this.EmpName);
+            Console.WriteLine("Dept Name:" + this.DeptName);
+            Console.WriteLine("Salary:" + this.Salary);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e=new Emp();
+            int eid;
+            string en, dn;
+            double s;
+
+            Console.Write("Enter EmpID:");
+            eid = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter EmpName:");
+            en= Console.ReadLine();
+            Console.Write("Enter DeptName:");
+            dn= Console.ReadLine();
+            Console.Write("Enter Salary:");
+            s=Convert.ToDouble(Console.ReadLine());
+
+            e.AddEmp(eid,en,dn,s);
+            e.DispEmp();
+        }
+    }
+}
---
+++ Practice/3-11-2024/ProductEX/ProductEX/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductEX
+{
+    class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string MfgName { get; set; }
+        public double Price { get; set; }
+
+        public void AddProduct(int pid,string pn,string mn,double pr)
+        {
+            this.ProductID = pid;
+            this.ProductName = pn;
+            this.MfgName = mn;
+            this.Price = pr;
+        }
+
+        public void DispProduct()
+        {
+            Console.WriteLine("ProductID:" + this.ProductID);
+            Console.WriteLine("Product Name:"+ this.ProductName);
+            Console.WriteLine("Mfg Name:" + this.MfgName);
+            Console.WriteLine("Price:"+ this.Price);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p=new Product();
+            p.AddProduct(1, "Mouse", "Intex", 450);
+            p.DispProduct();
+        }
+    }
+}
