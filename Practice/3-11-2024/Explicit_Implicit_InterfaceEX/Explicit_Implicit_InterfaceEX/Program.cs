1a021517befacee28d9080aba6726c4fbcce50b4
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-11-09 14:07:58 +0530

    9/11

---
+++ Practice/3-11-2024/Explicit_Implicit_InterfaceEX/Explicit_Implicit_InterfaceEX/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Implicit_InterfaceEX
+{
+    //Implicit interface implementation
+    /*interface Aint
+    {
+        void SayHello();
+    }
+    interface Bint
+    {
+        void SayHello();
+    }
+    class Message : Aint, Bint
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Say Hello Called..!");
+        }
+    }*/
+
+
+    //Explicit interface implementation
+    interface Aint
+    {
+        void SayHello();
+    }
+    interface Bint
+    {
+        void SayHello();
+    }
+    class Message : Aint, Bint
+    {
+        void Aint.SayHello()
+        {
+            Console.WriteLine("Say Hello for Aint Called..!");
+        }
+        void Bint.SayHello()
+        {
+            Console.WriteLine("Say Hello for Bint Called..!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit interface implementation
+            /*Message m =new Message();
+            m.SayHello();  */ 
+
+            //Explicit interface implementation
+            Message m=new Message();
+            Aint a = new Message();
+            a.SayHello();
+
+            Bint b=new Message();
+            b.SayHello();
+        }
+    }
+}
---
+++ Practice/3-11-2024/Get_Set_Prop_ProductEX/Get_Set_Prop_ProductEX/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_Set_Prop_ProductEX
+{
+    class Product
+    {
+        private int ProductID;
+        private string ProductName;
+        private string MfgName;
+        public double Price;
+        public double ProPrice
+        {
+            get { return Price; }
+        }
+        public Product(int pid,string pn,string mn,double p)
+        {
+            this.ProductID = pid;
+            this.ProductName = pn;
+            this.MfgName = mn;
+            this.Price = p;
+        }
+        public void DispProduct()
+        {
+            Console.WriteLine("ProductID is:"+ this.ProductID);
+            Console.WriteLine("Product Name is:" + this.ProductName);
+            Console.WriteLine("Mfg Name is:"+ this.MfgName);
+            Console.WriteLine("Price is:"+ this.Price);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> pr = new List<Product>();
+            int pid;
+            string pn, mn;
+            double p;
+            for(int i=0;i<3;i++)
+            {
+                Console.Write("Enter ProductID:");
+                pid = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter ProductName:");
+                pn=Console.ReadLine();
+                Console.Write("Enter MfgName:");
+                mn=Console.ReadLine();
+                Console.Write("Enter Price:");
+                p=Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine();
+
+                pr.Add(new Product(pid,pn,mn,p));
+            }
+            foreach(Product temp in pr)
+            {
+                if(temp.ProPrice>500)
+                {
+                    temp.DispProduct();
+                }
+            }
+        }
+    }
+}
