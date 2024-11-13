495596700f2738c7fcfbcd8086a9fd65d0998ba0
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-11-13 09:00:09 +0530

    13/11

---
+++ Practice/3-11-2024/ForLoopAddEX/ForLoopAddEX/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopAddEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sum = 0;
+            for(int i=0;i<5;i++)
+            {
+                Console.Write("Enter No:");
+                num=Convert.ToInt32(Console.ReadLine());
+                sum = sum + num;
+            }
+            Console.WriteLine("Sum of all digits is:"+ sum);    
+        }
+    }
+}
