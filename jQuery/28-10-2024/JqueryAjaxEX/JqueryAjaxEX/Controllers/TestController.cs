d9769358dcd82204bfaf3d52fd7b1298fcd19ffd
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-11-20 11:30:32 +0530

    20/11

---
+++ jQuery/28-10-2024/JqueryAjaxEX/JqueryAjaxEX/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using JqueryAjaxEX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryAjaxEX.Controllers
+{
+    public class TestController : Controller
+    { 
+        CommonEntities ce=new CommonEntities();
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmp()
+        {
+            return Json(this.ce.EmpTbls.ToList(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ jQuery/28-10-2024/JqueryAjaxEX/JqueryAjaxEX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JqueryAjaxEX.MvcApplication" Language="C#" %>
