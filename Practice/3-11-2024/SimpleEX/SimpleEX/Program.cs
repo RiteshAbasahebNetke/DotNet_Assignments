1241ab6e8cd5a2cc225e328fe8994aa0ebc79070
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-11-21 22:00:21 +0530

    21/11

---
+++ Practice/3-11-2024/SimpleEX/SimpleEX/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num,sq;
+            Console.Write("Enter a number:");
+            num=Convert.ToInt32(Console.ReadLine());
+            sq = num * num;
+            Console.WriteLine("Square is:"+ sq);    
+        }
+    }
+}
