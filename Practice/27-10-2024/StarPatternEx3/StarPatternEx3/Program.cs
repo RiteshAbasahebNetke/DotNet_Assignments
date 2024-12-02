1c0acf5638e7d95327c4fb0d47692559a3f70ab3
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-12-02 20:35:10 +0530

    2/12

---
+++ Practice/27-10-2024/StarPatternEx3/StarPatternEx3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatternEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //* Pattern
+            /*for(int i=1; i<=5; i++)
+            {
+                for(int j=4;j>=i;j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=1;k<=i;k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }*/
+
+            //Number Pattern
+            /*for(int i=0; i<=5; i++)
+            {
+                for(int j=4; j>=i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=1; k<=i; k++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }*/
+
+            for(int i=1; i<=5; i++)
+            {
+                for(int j=4; j>=i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for(int k=1; k<=i; k++)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
