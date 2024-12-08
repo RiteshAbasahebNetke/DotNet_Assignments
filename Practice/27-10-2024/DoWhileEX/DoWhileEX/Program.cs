971825cc09bc02cca38d528bde70d48ae90a4e2b
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-12-08 17:00:53 +0530

    8/12

---
+++ Practice/27-10-2024/DoWhileEX/DoWhileEX/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Replace Even Digit from number with 0
+
+            int num, dig;
+            Console.Write("Enter Number:");
+            num=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Output is:");
+            do
+            {
+                dig = num % 10;
+                if (dig % 2 == 0)
+                {
+                    Console.Write("0");
+                }
+                else
+                {
+                    Console.Write(dig);
+                }
+                num = num / 10;
+            } while (num > 0);
+
+        }
+    }
+}
---
+++ Practice/27-10-2024/PrimeNumbersEX/PrimeNumbersEX/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prime No. from 1 to 100
+            int i, j, k;
+            for(i=1;i<=100;i++)
+            {
+                k = 0;
+                for(j=2;j<i;j++)
+                {
+                    if(i%j==0)
+                    {
+                        k= 1;
+                        break;
+                    }
+                }
+                if (k == 0)
+                {
+                    Console.WriteLine("Prime:" + i);
+                }
+            }
+            
+        }
+    }
+}
