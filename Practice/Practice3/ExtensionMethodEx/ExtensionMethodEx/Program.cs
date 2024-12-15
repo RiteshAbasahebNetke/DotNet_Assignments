4de98f1b32614ce221adbe9ec18e5ad1445c7c2b
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-12-15 23:05:04 +0530

    15/12

---
+++ Practice/Practice3/ExtensionMethodEx/ExtensionMethodEx/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int GetCapCount(this string obj)
+            {
+                int count = 0;
+                for(int a=0;a<=obj.Length;a++)
+                {
+                    if(obj[a] == 'A' && obj[a]<='Z')
+                        count++;
+                }
+                return count;
+            }
+        }
+    }
+}
---
+++ Practice/Practice3/GenericEx/GenericEx/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericEx
+{
+    class Search<T>
+    {
+        public bool doSearch(T[] data,T d)
+        {
+            bool result = false;
+            foreach(var temp in data)
+            {
+                if(temp.Equals(d))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Search<int> s=new Search<int>();
+
+            int[] data = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            if(s.doSearch(data,9))
+            {
+                Console.WriteLine("Number Found");
+            }
+            else
+            {
+                Console.WriteLine("Number Not Found");
+            }
+
+            Search<string> st=new Search<string>();
+
+            string[] names = { "Rajesh", "Ramesh", "Raju" };
+            if(st.doSearch(names,"Rajesh"))
+            {
+                Console.WriteLine("Name Found");
+            }
+            else
+            {
+                Console.WriteLine("Name Not Found");
+            }
+        }
+    }
+}
