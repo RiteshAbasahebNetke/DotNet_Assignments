46b61869ec3457b3f8233620204780a624df9e1e
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-12-20 20:55:10 +0530

    20/12

---
+++ Practice/Practice1/ObjectSlicingEx/ObjectSlicingEx/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectSlicingEx
+{
+    class Parent
+    {
+        public virtual void DispMethod()
+        {
+            Console.WriteLine("Parent Class");
+        }
+    }
+    class Child:Parent
+    {
+        public override void DispMethod()
+        {
+            Console.WriteLine("Child Class");  
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Parent();
+            Child c=new Child();
+            p = c;
+            p.DispMethod();
+        }
+    }
+}
