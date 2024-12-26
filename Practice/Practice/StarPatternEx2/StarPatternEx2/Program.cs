7c142640ff0139ee04dbe390870dd99370155bda
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2024-12-26 17:00:20 +0530

    26/12

---
+++ Practice/Practice/StarPatternEx2/StarPatternEx2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatternEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // * Pattern
+            /*for(int i=1; i<=5; i++)
+            {
+                for(int j=5; j>=i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }*/
+
+            //Number Pattern
+            /*for(int i=1; i<=5; i++)
+            {
+                for(int j=5; j>=i;j--)
+                {
+                    Console.Write(i+ " ");
+                }
+                Console.WriteLine();
+            }*/
+
+            for(int i=1; i<=5; i++)
+            {
+                for(int j=5;j>=i; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Practice/Practice/StarPatternEx5/StarPatternEx5/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarPatternEx5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // * Pattern
+            /*for(int i=5; i>=1;i--)
+            {
+                for(int k=4;k>=i;k--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=i;j>=1;j--)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }*/
+
+            // Number Pattern
+            /*for(int i=5;i>=1;i--)
+            {
+                for(int k=4;k>=i;k--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=i;j>=1;j--)
+                {
+                    Console.Write(i+" ");
+                }
+                Console.WriteLine();
+            }*/
+
+            for(int i=5;i>=1;i--)
+            {
+                for(int k=4;k>=i;k--)
+                {
+                    Console.Write(" ");
+                }
+                for(int j=i;j>=1;j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
