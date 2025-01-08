c9568cc0d0f6f429e7d5b001afbf566c2406d3ce
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-01-08 21:24:28 +0530

    8/1

---
+++ Practice/Practice3/Armstrong_No/Armstrong_No/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong_No
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, dig, sum;
+            for(int i = 1; i < 500; i++)
+            {
+                num = i;
+                sum = 0;
+                while (num > 0)
+                {
+                    dig = num % 10;
+                    sum = sum + (dig * dig * dig);
+                    num = num / 10;
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
