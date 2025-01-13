953ef3cf177e5c641517722c7ff2e32f058dcf5c
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-01-13 17:04:06 +0530

    13/1

---
+++ Practice/Practice3/ExplicitIntrfaceEx/ExplicitIntrfaceEx/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitIntrfaceEx
+{
+    interface AInt
+    {
+        void SayHello();
+    }
+    interface BInt
+    {
+        void SayHello();
+    }
+
+    class Message : AInt, BInt
+    {
+         void AInt.SayHello()
+        {
+            Console.WriteLine("AInt Called..!");
+        }
+        void BInt.SayHello()
+        {
+            Console.WriteLine("BInt Called..!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AInt a=new Message();
+            a.SayHello();
+
+            BInt b = new Message();
+            b.SayHello();
+        }
+    }
+}
