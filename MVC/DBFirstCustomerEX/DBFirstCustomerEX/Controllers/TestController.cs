873866d091d6be136b6baa0b744dea1097fc9ab3
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-01-16 17:08:03 +0530

    16/1

---
+++ MVC/DBFirstCustomerEX/DBFirstCustomerEX/Controllers/TestController.cs
@@ -0,0 +1,68 @@
+﻿using DBFirstCustomerEX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBFirstCustomerEX.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        CurdOfCoreEntities cc=new CurdOfCoreEntities();
+        public ActionResult Index()
+        {
+            return View(this.cc.CustomerTbls.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.AreaID = new SelectList(this.cc.AreaTbls.ToList(), "AreaID", "AreaName");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CustomerTbl rec)
+        {
+            if(ModelState.IsValid)
+            {
+                this.cc.CustomerTbls.Add(rec);
+                this.cc.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AreaID = new SelectList(this.cc.AreaTbls.ToList(), "AreaID", "AreaName");
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Int64 id)
+        {
+            ViewBag.AreaID = new SelectList(this.cc.AreaTbls.ToList(), "AreaID", "AreaName");
+            var rec = this.cc.CustomerTbls.Find(id);
+            return View(rec);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CustomerTbl rec)
+        {
+            if (ModelState.IsValid)
+            {
+                this.cc.Entry(rec).State=System.Data.EntityState.Modified;
+                this.cc.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AreaID = new SelectList(this.cc.AreaTbls.ToList(), "AreaID", "AreaName");
+            return View(rec);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(Int64 id)
+        {
+            var rec = this.cc.CustomerTbls.Find(id);
+            this.cc.CustomerTbls.Remove(rec);
+            this.cc.SaveChanges();
+            return RedirectToAction("Index");   
+        }
+    }
+}
---
+++ MVC/DBFirstCustomerEX/DBFirstCustomerEX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DBFirstCustomerEX.MvcApplication" Language="C#" %>
