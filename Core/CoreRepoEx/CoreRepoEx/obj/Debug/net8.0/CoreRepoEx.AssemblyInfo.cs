979dfdd7c7ba8e77563aa4fd4f233a98e0d5c68b
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-01-19 20:22:53 +0530

    19/1

--- Core/CoreRepoEx/CoreRepoEx/obj/Debug/net8.0/CoreRepoEx.AssemblyInfo.cs
+++ Core/CoreRepoEx/CoreRepoEx/obj/Debug/net8.0/CoreRepoEx.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreRepoEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9602df42ca566580e73ada2c313e81e18d5feb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoreRepoEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoreRepoEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
