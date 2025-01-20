ee90c23e11f9355d30acd5294ee6cd8964615c3c
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-01-20 20:32:12 +0530

    20/1

---
+++ Practice/Practice3/R_Example/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Practice
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, dig, rno=0;
+            Console.Write("Enter Number:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num > 0)
+            {
+                dig = num % 10;
+                rno = dig+(rno*10);
+                num = num / 10;
+            }
+            Console.WriteLine("Sum of digits is:" + rno);
+        }
+    }
+}
