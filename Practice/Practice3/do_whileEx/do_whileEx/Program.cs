9ad852eaec3a14ae714203ebd5c7f11e846c4f1e
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-02-03 19:57:44 +0530

    3/2

---
+++ Practice/Practice3/do_whileEx/do_whileEx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace do_whileEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, ocnt = 0;
+            do
+            {
+                Console.Write("Enter No:");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (!(num%2==0))
+                {
+                    ocnt++;
+                }
+            } while (!(num % 8 == 0));
+            Console.WriteLine("Count is:"+ ocnt);
+        }
+    }
+}
