42b95ff25958cefe37057c393a163bbb15ad0b01
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-02-21 19:59:57 +0530

    21/2

---
+++ Practice/Practice/GetSetExample/GetSetExample/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetExample
+{
+    class Emp
+    {
+        private int EmpID;
+        private string EmpName;
+        private string DeptName;
+        private double Salary;
+        public string propdeptname
+        {
+            get { return DeptName; }
+        }
+        public double propsalary
+        {
+            get { return Salary; }
+        }
+
+        public Emp(int eid,string en,string dn,double s)
+        {
+            this.EmpID = eid;
+            this.EmpName = en;
+            this.DeptName = dn;
+            this.Salary = s;
+        }
+        public void dispEmp()
+        {
+            Console.WriteLine("EmpID:" + this.EmpID);
+            Console.WriteLine("EmpName:"+ this.EmpName);
+            Console.WriteLine("DeptName:" + this.DeptName);
+            Console.WriteLine("Salary:" + this.Salary);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emp> el = new List<Emp>();
+            int eid;
+            string en, dn;
+            double s;
+
+            for(int i=0;i<3;i++)
+            {
+                Console.Write("Enter EmpID:");
+                eid=Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter EmpName:");
+                en = Console.ReadLine();
+
+                Console.Write("Enter DeptName:");
+                dn = Console.ReadLine();
+
+                Console.Write("Enter Salary:");
+                s=Convert.ToDouble(Console.ReadLine());
+
+                el.Add(new Emp(eid,en,dn,s));
+            }
+            Console.WriteLine("***************Emp Details:******************");
+            foreach(Emp temp in el)
+            {
+                if(temp.propsalary>15000 && temp.propdeptname=="Sales")
+                {
+                    temp.dispEmp();
+                }
+            }
+        }
+    }
+}
---
+++ Practice/Practice/GetSetProductEX/GetSetProductEX/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetSetProductEX
+{
+    class Product
+    {
+        private int ProductID;
+        private string ProductName;
+        private string MfgName;
+        private double Price;
+
+        public double propPrice
+        {
+            get { return Price; }
+        }
+
+        public Product(int pid, string pn, string mn, double p)
+        {
+            this.ProductID= pid;
+            this.ProductName = pn;
+            this.MfgName = mn;
+            this.Price = p;
+        }
+        public void dispProduct()
+        {
+            Console.WriteLine("ProductID:" + this.ProductID);
+            Console.WriteLine("ProductName:" + this.ProductName);
+            Console.WriteLine("MfgName:" + this, MfgName);
+            Console.WriteLine("Price:" + this.Price);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> pl = new List<Product>();
+            int pid;
+            string pn, mn;
+            double p;
+
+            for(int i=0;i<3;i++)
+            {
+                Console.Write("Enter ProductID:");
+                pid = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter ProductName:");
+                pn = Console.ReadLine();
+
+                Console.Write("Enter MfgName:");
+                mn = Console.ReadLine();
+
+                Console.WriteLine("Enter Price:");
+                p = Convert.ToDouble(Console.ReadLine());
+
+                pl.Add(new Product(pid, pn, mn, p));
+            }
+            Console.WriteLine("*************Product Detail:***************");
+            foreach(Product temp in pl)
+            {
+                if(temp.propPrice>500)
+                {
+                    temp.dispProduct();
+                }
+            }
+        }
+    }
+}
