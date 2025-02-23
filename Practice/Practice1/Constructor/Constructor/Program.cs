a52766421327ffbdec91c82defd2c0608a688bf9
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-02-23 20:04:28 +0530

    23/2

---
+++ Practice/Practice1/Constructor/Constructor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Course        
+    {
+        private int CourseID;
+        private string CourseName;
+        private double CourseDuration;
+        private double CourseFees;
+
+        public Course()     // Default Constructor
+        {
+            CourseID = 1;
+            CourseName = "C#";
+            CourseDuration = 3;
+            CourseFees = 4000;
+        }
+        public Course(int cid,string cn)     //Constructor with 2 parameters
+        {
+            CourseID = cid;
+            CourseName = cn;
+        }
+        public void DispCourse() 
+        {
+            Console.WriteLine("CourseID:" + CourseID);
+            Console.WriteLine("CourseName:" + CourseName);
+            Console.WriteLine("CourseDuration:" + CourseDuration);
+            Console.WriteLine("CourseFees:" + CourseFees);
+        } 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Course c = new Course();
+            c.DispCourse();
+
+            Course c1 = new Course(2,"VB");
+            c1.DispCourse();
+        }
+    }
+}
---
+++ Practice/Practice1/ExceptionEx/ExceptionEx/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExceptionEx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExceptionEx")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7a94407-6125-46eb-9d8d-9562698c6d04")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
