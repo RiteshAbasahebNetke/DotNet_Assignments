8bbc33e84b91d5969d3f58f8f35a97e0736cfbea
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-02-28 20:20:47 +0530

    28/2

---
+++ Practice/Practice4/InterfaceEX/InterfaceEX/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceEX
+{
+    interface AreaInt
+    {
+        double CalculateArea();
+    }
+    class Circle : AreaInt
+    {
+        private double r;
+        public Circle(double r)
+        {
+            this.r = r;
+        }
+        public double CalculateArea()
+        {
+            double area = 3.14 * r * r;
+            return area;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle(4);
+            double area = c.CalculateArea();
+            Console.WriteLine("Area of Circle is:" + area);
+        }
+    }
+}
