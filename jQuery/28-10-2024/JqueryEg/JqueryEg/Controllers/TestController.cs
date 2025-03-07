2f8114719f78b5b8cbe130034fe3d1f7d176804e
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-03-07 20:30:59 +0530

    7/3

---
+++ jQuery/28-10-2024/JqueryEg/JqueryEg/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryEg.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult FewMoreSelectors()
+        {
+            return View();
+        }
+        public ActionResult DomTraversal()
+        {
+            return View();
+        }
+        public ActionResult DomTraversal1()
+        {
+            return View();
+        }
+        public ActionResult Event()
+        {
+            return View();
+        }
+    }
+}
---
+++ jQuery/28-10-2024/JqueryEg/JqueryEg/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JqueryEg.MvcApplication" Language="C#" %>
