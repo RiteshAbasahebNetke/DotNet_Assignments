39ded36054ed7018565a8a5b11d64b460f85583f
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-03-14 20:12:54 +0530

    14/3

---
+++ Practice/Practice4/OperatorOverloadingEx/OperatorOverloadingEx/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingEx
+{
+    class Point
+    {
+        private int x;
+        private int y;
+        public void intPoint(int a,int b)
+        {
+            this.x = a;
+            this.y = b;
+        }
+        public void dispPoint()
+        {
+            Console.WriteLine("X:" + this.x);
+            Console.WriteLine("Y:" + this.y);
+        }
+        public static Point operator ++(Point t)
+        {
+            t.x++;
+            t.y++;
+            return t;
+        }
+        public static Point operator --(Point t)
+        {
+            t.x--;
+            t.y--;
+            return t;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p=new Point();
+            p.intPoint(10, 20);
+            p++;
+            p.dispPoint();
+
+            Point p1 = new Point();
+            p1.intPoint(10, 20);
+            p1.dispPoint();
+            p1--;
+        }
+    }
+}
