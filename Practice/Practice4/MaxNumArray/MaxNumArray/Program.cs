7ee4a2333e8628d4a39e04e1fcb8dd22acb61ea6
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-03-17 18:03:49 +0330

    17/3

---
+++ Practice/Practice4/MaxNumArray/MaxNumArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[5];
+            for(int i=0;i<5;i++)
+            {
+                Console.Write("Enter No:");
+                n[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int max = n[0];
+            for (int i = 0; i < 5; i++)
+            {
+                if (max < n[i])
+                {
+                    max = n[i];
+                }
+                
+            }
+            Console.WriteLine("Maximum is:" + max);
+        }
+    }
+}
