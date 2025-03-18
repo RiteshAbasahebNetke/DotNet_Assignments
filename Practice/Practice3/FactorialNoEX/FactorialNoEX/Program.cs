6d27f9bc2a215a4a5ed17b3f3ead98c5cf244ca8
RiteshAbasahebNetke <ritesh.netke@outlook.com>
2025-03-18 13:21:05 +0330

    18/3

---
+++ Practice/Practice3/FactorialNoEX/FactorialNoEX/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNoEX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, fact = 1;
+
+            Console.Write("Enter Number:");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            for(int i=1; i<=n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of" + n + "is" + fact);
+        }
+    }
+}
